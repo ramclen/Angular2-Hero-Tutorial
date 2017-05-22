7e0030bfd341f27bc5f6e022e4040c9099a06b0b
Cristopher López Santana <cristopher.correo@gmail.com>
2017-05-22 15:32:49 +0100

    Forth Step (Services) Implementation of service and working with promises, constructors and hook life cycles (ngOnInit)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from "./Hero";
+import {HeroService} from "./hero.service";
 
 @Component({
   selector: 'app-root',
+  providers: [HeroService],
   template:  `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
@@ -69,24 +71,24 @@ import {Hero} from "./Hero";
   `]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Tour of Heroes';
   selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  heroes = HEROES;
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes=> this.heroes = heroes);
+  }
 }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+
---
+++ src/app/hero.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {Hero} from "./Hero";
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]>{
+    return Promise.resolve(HEROES);
+  }
+
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
+  }
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
